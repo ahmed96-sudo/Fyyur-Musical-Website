c97a49f0420dd09205077695e3146b73299c133d
Ahmed <asyd12855@gmail.com>
2021-02-18 11:05:20 +0200

    fyyer

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ import json
 import logging
 import os
 import sys
-from logging import FileHandler, Formatter
+#from logging import FileHandler, Formatter
 import babel
 import dateutil.parser
 from flask import Flask, Response, flash, jsonify, redirect, render_template,request, url_for
 #from flask_migrate import Migrate
-from flask_moment import Moment
+#from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import Form
 from forms import *
@@ -23,7 +23,7 @@ from models import db, Artist, Venue, Show
 
 
 app = Flask(__name__)
-moment = Moment(app)
+#moment = Moment(app)
 app.config.from_object('config')
 db.init_app(app)
 #migrate = Migrate(app, db)
@@ -391,16 +391,16 @@ def server_error(error):
     return render_template('errors/500.html'), 500
 
 
-if not app.debug:
-    file_handler = FileHandler('error.log')
-    file_handler.setFormatter(
-        Formatter(
-            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    )
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('errors')
+#if not app.debug:
+#    file_handler = FileHandler('error.log')
+#    file_handler.setFormatter(
+#        Formatter(
+#            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+#    )
+#    app.logger.setLevel(logging.INFO)
+#    file_handler.setLevel(logging.INFO)
+#    app.logger.addHandler(file_handler)
+#    app.logger.info('errors')
 
 # Launch.
 
