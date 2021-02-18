1653cf296268254c076d9436b87b2219bec2a716
Ahmed <asyd12855@gmail.com>
2021-02-18 10:43:36 +0200

    fyyer

--- app.py
+++ app.py
@@ -28,8 +28,7 @@ app.config.from_object('config')
 db.init_app(app)
 #migrate = Migrate(app, db)
 
-SQLALCHEMY_DATABASE_URI = 'postgres://mfvwyllgqwiabj:95f8e274ebedbf79a8a83e177ca37615bc5bb8c63152d863cdf6627f8e531138@ec2-35-174-118-71.compute-1.amazonaws.com:5432/dv76fksc0gb6d
-'
+SQLALCHEMY_DATABASE_URI = 'postgres://mfvwyllgqwiabj:95f8e274ebedbf79a8a83e177ca37615bc5bb8c63152d863cdf6627f8e531138@ec2-35-174-118-71.compute-1.amazonaws.com:5432/dv76fksc0gb6d'
 # Filters.
 
 
--- config.py
+++ config.py
@@ -10,6 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://mfvwyllgqwiabj:95f8e274ebedbf79a8a83e177ca37615bc5bb8c63152d863cdf6627f8e531138@ec2-35-174-118-71.compute-1.amazonaws.com:5432/dv76fksc0gb6d
-'
+SQLALCHEMY_DATABASE_URI = 'postgres://mfvwyllgqwiabj:95f8e274ebedbf79a8a83e177ca37615bc5bb8c63152d863cdf6627f8e531138@ec2-35-174-118-71.compute-1.amazonaws.com:5432/dv76fksc0gb6d'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
