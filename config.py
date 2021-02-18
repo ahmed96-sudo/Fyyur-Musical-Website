deffb738a60f997af0f9b6bb0ccd38b5ff2b7b14
Ahmed <asyd12855@gmail.com>
2021-02-18 12:18:58 +0200

    fyyur-app

--- config.py
+++ config.py
@@ -3,12 +3,10 @@ SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Enable debug mode.
+# Enable debug mode.  Must run with "python app.py" for this to be picked up
 DEBUG = True
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Connect to the database
-
-
-# TODO IMPLEMENT DATABASE URL
+# toy password for assignment (Windows required it), not real world best practice!
 SQLALCHEMY_DATABASE_URI = 'postgres://mfvwyllgqwiabj:95f8e274ebedbf79a8a83e177ca37615bc5bb8c63152d863cdf6627f8e531138@ec2-35-174-118-71.compute-1.amazonaws.com:5432/dv76fksc0gb6d'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
--- forms.py
+++ forms.py
@@ -1,92 +1,9 @@
 from datetime import datetime
-from flask_wtf import Form
-from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, TextAreaField, BooleanField
-from wtforms.validators import DataRequired, AnyOf, URL, Regexp, ValidationError
-import re
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField
+from wtforms.validators import DataRequired, AnyOf, URL, Optional
 
-
-def validate_phone(form, field):
-    if not re.search(r"^[0-9]*$", field.data):
-        raise ValidationError("Phone number should only contain digits.")
-
-
-genres_choices = [
-    ('Alternative', 'Alternative'),
-    ('Blues', 'Blues'),
-    ('Classical', 'Classical'),
-    ('Country', 'Country'),
-    ('Electronic', 'Electronic'),
-    ('Folk', 'Folk'),
-    ('Funk', 'Funk'),
-    ('Hip-Hop', 'Hip-Hop'),
-    ('Heavy Metal', 'Heavy Metal'),
-    ('Instrumental', 'Instrumental'),
-    ('Jazz', 'Jazz'),
-    ('Musical Theatre', 'Musical Theatre'),
-    ('Pop', 'Pop'),
-    ('Punk', 'Punk'),
-    ('R&B', 'R&B'),
-    ('Reggae', 'Reggae'),
-    ('Rock n Roll', 'Rock n Roll'),
-    ('Soul', 'Soul'),
-    ('Other', 'Other'),
-]
-state_choices = [
-    ('AL', 'AL'),
-    ('AK', 'AK'),
-    ('AZ', 'AZ'),
-    ('AR', 'AR'),
-    ('CA', 'CA'),
-    ('CO', 'CO'),
-    ('CT', 'CT'),
-    ('DE', 'DE'),
-    ('DC', 'DC'),
-    ('FL', 'FL'),
-    ('GA', 'GA'),
-    ('HI', 'HI'),
-    ('ID', 'ID'),
-    ('IL', 'IL'),
-    ('IN', 'IN'),
-    ('IA', 'IA'),
-    ('KS', 'KS'),
-    ('KY', 'KY'),
-    ('LA', 'LA'),
-    ('ME', 'ME'),
-    ('MT', 'MT'),
-    ('NE', 'NE'),
-    ('NV', 'NV'),
-    ('NH', 'NH'),
-    ('NJ', 'NJ'),
-    ('NM', 'NM'),
-    ('NY', 'NY'),
-    ('NC', 'NC'),
-    ('ND', 'ND'),
-    ('OH', 'OH'),
-    ('OK', 'OK'),
-    ('OR', 'OR'),
-    ('MD', 'MD'),
-    ('MA', 'MA'),
-    ('MI', 'MI'),
-    ('MN', 'MN'),
-    ('MS', 'MS'),
-    ('MO', 'MO'),
-    ('PA', 'PA'),
-    ('RI', 'RI'),
-    ('SC', 'SC'),
-    ('SD', 'SD'),
-    ('TN', 'TN'),
-    ('TX', 'TX'),
-    ('UT', 'UT'),
-    ('VT', 'VT'),
-    ('VA', 'VA'),
-    ('WA', 'WA'),
-    ('WV', 'WV'),
-    ('WI', 'WI'),
-    ('WY', 'WY'),
-]
-
-
-class ShowForm(Form):
+class ShowForm(FlaskForm):
     artist_id = StringField(
         'artist_id'
     )
@@ -96,11 +13,10 @@ class ShowForm(Form):
     start_time = DateTimeField(
         'start_time',
         validators=[DataRequired()],
-        default=datetime.today()
+        default= datetime.today()
     )
 
-
-class VenueForm(Form):
+class VenueForm(FlaskForm):
     name = StringField(
         'name', validators=[DataRequired()]
     )
@@ -109,37 +25,111 @@ class VenueForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=state_choices
+        choices=[
+            ('AL', 'AL'),
+            ('AK', 'AK'),
+            ('AZ', 'AZ'),
+            ('AR', 'AR'),
+            ('CA', 'CA'),
+            ('CO', 'CO'),
+            ('CT', 'CT'),
+            ('DE', 'DE'),
+            ('DC', 'DC'),
+            ('FL', 'FL'),
+            ('GA', 'GA'),
+            ('HI', 'HI'),
+            ('ID', 'ID'),
+            ('IL', 'IL'),
+            ('IN', 'IN'),
+            ('IA', 'IA'),
+            ('KS', 'KS'),
+            ('KY', 'KY'),
+            ('LA', 'LA'),
+            ('ME', 'ME'),
+            ('MT', 'MT'),
+            ('NE', 'NE'),
+            ('NV', 'NV'),
+            ('NH', 'NH'),
+            ('NJ', 'NJ'),
+            ('NM', 'NM'),
+            ('NY', 'NY'),
+            ('NC', 'NC'),
+            ('ND', 'ND'),
+            ('OH', 'OH'),
+            ('OK', 'OK'),
+            ('OR', 'OR'),
+            ('MD', 'MD'),
+            ('MA', 'MA'),
+            ('MI', 'MI'),
+            ('MN', 'MN'),
+            ('MS', 'MS'),
+            ('MO', 'MO'),
+            ('PA', 'PA'),
+            ('RI', 'RI'),
+            ('SC', 'SC'),
+            ('SD', 'SD'),
+            ('TN', 'TN'),
+            ('TX', 'TX'),
+            ('UT', 'UT'),
+            ('VT', 'VT'),
+            ('VA', 'VA'),
+            ('WA', 'WA'),
+            ('WV', 'WV'),
+            ('WI', 'WI'),
+            ('WY', 'WY'),
+        ]
     )
     address = StringField(
         'address', validators=[DataRequired()]
     )
     phone = StringField(
-        'phone', validators=[validate_phone]
+        'phone', validators=[DataRequired()]
     )
     image_link = StringField(
-        'image_link'
+        'image_link', validators=[Optional(), URL()]    # Can chain these
+    )
+    website = StringField(
+        'website', validators=[Optional(), URL()]
+    )
+    seeking_talent = SelectField(
+        'seeking_talent', validators=[DataRequired()],
+        choices=[
+            ('Yes', 'Yes'),
+            ('No', 'No')
+        ]
+    )
+    seeking_description = StringField(
+        'seeking_description', validators=[Optional()]
     )
     genres = SelectMultipleField(
         'genres', validators=[DataRequired()],
-        choices=genres_choices
+        choices=[
+            ('Alternative', 'Alternative'),
+            ('Blues', 'Blues'),
+            ('Classical', 'Classical'),
+            ('Country', 'Country'),
+            ('Electronic', 'Electronic'),
+            ('Folk', 'Folk'),
+            ('Funk', 'Funk'),
+            ('Hip-Hop', 'Hip-Hop'),
+            ('Heavy Metal', 'Heavy Metal'),
+            ('Instrumental', 'Instrumental'),
+            ('Jazz', 'Jazz'),
+            ('Musical Theatre', 'Musical Theatre'),
+            ('Pop', 'Pop'),
+            ('Punk', 'Punk'),
+            ('R&B', 'R&B'),
+            ('Reggae', 'Reggae'),
+            ('Rock n Roll', 'Rock n Roll'),
+            ('Soul', 'Soul'),
+            ('Other', 'Other'),
+        ]
     )
     facebook_link = StringField(
-        'facebook_link', validators=[URL()]
-    )
-    website = StringField(
-        'website', validators=[URL()]
-    )
-    seeking_talent = BooleanField(
-        'seeking_talent'
-    )
-    seeking_description = StringField(
-        'seeking_description'
-
+        'facebook_link', validators=[Optional(), URL()]
     )
 
-
-class ArtistForm(Form):
+class ArtistForm(FlaskForm):
     name = StringField(
         'name', validators=[DataRequired()]
     )
@@ -148,28 +138,103 @@ class ArtistForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=state_choices
+        choices=[
+            ('AL', 'AL'),
+            ('AK', 'AK'),
+            ('AZ', 'AZ'),
+            ('AR', 'AR'),
+            ('CA', 'CA'),
+            ('CO', 'CO'),
+            ('CT', 'CT'),
+            ('DE', 'DE'),
+            ('DC', 'DC'),
+            ('FL', 'FL'),
+            ('GA', 'GA'),
+            ('HI', 'HI'),
+            ('ID', 'ID'),
+            ('IL', 'IL'),
+            ('IN', 'IN'),
+            ('IA', 'IA'),
+            ('KS', 'KS'),
+            ('KY', 'KY'),
+            ('LA', 'LA'),
+            ('ME', 'ME'),
+            ('MT', 'MT'),
+            ('NE', 'NE'),
+            ('NV', 'NV'),
+            ('NH', 'NH'),
+            ('NJ', 'NJ'),
+            ('NM', 'NM'),
+            ('NY', 'NY'),
+            ('NC', 'NC'),
+            ('ND', 'ND'),
+            ('OH', 'OH'),
+            ('OK', 'OK'),
+            ('OR', 'OR'),
+            ('MD', 'MD'),
+            ('MA', 'MA'),
+            ('MI', 'MI'),
+            ('MN', 'MN'),
+            ('MS', 'MS'),
+            ('MO', 'MO'),
+            ('PA', 'PA'),
+            ('RI', 'RI'),
+            ('SC', 'SC'),
+            ('SD', 'SD'),
+            ('TN', 'TN'),
+            ('TX', 'TX'),
+            ('UT', 'UT'),
+            ('VT', 'VT'),
+            ('VA', 'VA'),
+            ('WA', 'WA'),
+            ('WV', 'WV'),
+            ('WI', 'WI'),
+            ('WY', 'WY'),
+        ]
     )
-
     phone = StringField(
-        'phone', validators=[DataRequired(), Regexp("^[0-9]*$", message="Phone number should only contain digits")]
+        'phone', validators=[DataRequired()]
     )
-
     image_link = StringField(
-        'image_link'
+        'image_link', validators=[Optional(), URL()]    # Can chain these
+    )
+    website = StringField(
+        'website', validators=[Optional(), URL()]
+    )
+    seeking_venue = SelectField(
+        'seeking_venue', validators=[DataRequired()],
+        choices=[
+            ('Yes', 'Yes'),
+            ('No', 'No')
+        ]
+    )
+    seeking_description = StringField(
+        'seeking_description', validators=[Optional()]
     )
     genres = SelectMultipleField(
         'genres', validators=[DataRequired()],
-        choices=genres_choices
+        choices=[
+            ('Alternative', 'Alternative'),
+            ('Blues', 'Blues'),
+            ('Classical', 'Classical'),
+            ('Country', 'Country'),
+            ('Electronic', 'Electronic'),
+            ('Folk', 'Folk'),
+            ('Funk', 'Funk'),
+            ('Hip-Hop', 'Hip-Hop'),
+            ('Heavy Metal', 'Heavy Metal'),
+            ('Instrumental', 'Instrumental'),
+            ('Jazz', 'Jazz'),
+            ('Musical Theatre', 'Musical Theatre'),
+            ('Pop', 'Pop'),
+            ('Punk', 'Punk'),
+            ('R&B', 'R&B'),
+            ('Reggae', 'Reggae'),
+            ('Rock n Roll', 'Rock n Roll'),
+            ('Soul', 'Soul'),
+            ('Other', 'Other'),
+        ]
     )
     facebook_link = StringField(
-        'facebook_link', validators=[URL()]
+        'facebook_link', validators=[Optional(), URL()]    # Can chain these
     )
-
-    website = StringField(
-        'website',  validators=[URL()]
-    )
-
-    seeking_venue = BooleanField('seeking_venue')
-
-    seeking_description = StringField('seeking_description')
